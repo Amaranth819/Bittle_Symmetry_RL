17b13a1d34454349cdd83d388ae24acbd5f1c82f
Amaranth819 <xulin.chen@outlook.com>
2024-08-28 02:17:23 -0400

    8.27: Fix some bugs in foot periodicity

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
-python rl_games_train.py -p -r -f rl_games_bittle.yaml
-# python rl_games_train.py -p -r -f rl_games_bittle.yaml -c runs/Bittle2024-08-26-01:08:27/nn/Bittle.pth
+python rl_games_train.py -p -r -f rl_games_bittle.yaml # Play the latest trained policy and record a video.
+# python rl_games_train.py -p -f rl_games_bittle.yaml # Play the latest trained policy with IsaacGym GUI.
+# python rl_games_train.py -p -r -f rl_games_bittle.yaml -c runs/Bittle2024-08-26-01:08:27/nn/Bittle.pth # Play the given policy and record a video.
